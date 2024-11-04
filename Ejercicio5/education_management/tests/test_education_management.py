f4b71ed6d6f5784a41e52cd2fe42a847c5f4f72a
siok <eladiohv2@gmail.com>
2024-11-04 19:07:40 +0000

    test

--- Ejercicio5/education_management/tests/test_education_management.py
+++ Ejercicio5/education_management/tests/test_education_management.py
@@ -9,25 +9,3 @@ def test_add_course():
     course = Course(1, "Math", "Algebra", "Dr. jose")
     system.manage_courses("add", course)
     assert 1 in system.courses
-
-def test_add_user():
-    system = EducationManagement()
-    user = User(1, "Alice", "alice@example.com", "password", "student")
-    system.manage_users("add", user)
-    assert 1 in system.users
-
-def test_enroll_student_in_course():
-    system = EducationManagement()
-    course = Course(1, "Math", "Algebra basics", "Dr. Smith")
-    user = User(1, "Alice", "alice@example.com", "password", "student")
-    system.manage_courses("add", course)
-    system.manage_users("add", user)
-    course.enroll_student(user.user_id)
-    assert user.user_id in course.enrolled_students
-
-def test_add_lesson_to_course():
-    course = Course(1, "Math", "Algebra basics", "Dr. Smith")
-    lesson = Lesson(1, "Lesson 1", "Introduction to Algebra", 30)
-    course.add_lesson(lesson)
-    assert len(course.lessons) == 1
-    assert course.lessons[0].title == "Lesson 1"
